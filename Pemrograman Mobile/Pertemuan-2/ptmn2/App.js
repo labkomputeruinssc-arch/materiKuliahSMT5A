fe229f1f1fe1dc2190b843c6119e96658278aaf8
mohamadfirdaus0305-hue <mohamadfirdaus0305@gmail.com>
2026-09-21 11:07:28 +0700

    Modul 3

--- Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
+++ Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
@@ -1,5 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Import Library
+import React, { useState } from 'react';
+// Import Componnent
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  SectionList,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  Switch,
+  Modal,
+  ActivityIndicator,
+  StatusBar,
+  SafeAreaView,
+  StyleSheet,
+  Alert,
+  Platform
+} from 'react';
+
+const PROFILE = {
+  name: 'Mohamad Firdaus',
+  title: 'Full Stack Mobile Developer',
+  email: 'mohamadfirdaus@uinssc.ac.id',
+  phone: '082170735116',
+  location: 'Cirebon, Jawa Barat',
+  bio: 'Pengembang aplikasi Mobile yang berfokus pada React Native dan Flutter',
+  avatar: 'https://lh5.googleusercontent.com/-mwBhriW4lBQ/AAAAAAAAAAI/AAAAAAAAAAA/Phl4QV1LsdY/s64-c-mo/photo.jpg',
+  avatarOffline: 'assets/photo.png'
+};
+
+
+
+
+
+
 
 export default function App() {
   return (
---
+++ Pemrograman Mobile/Pertemuan-3/Laporan-praktikum3.md
@@ -0,0 +1,34 @@
+# 📱Modul Praktikum 4: Core Components & Styling #
+
+## 🎯 Tujuan Pembelajaran
+
+Setelah menyelesaikan praktikum ini, mahasiswa mampu:
+
+1. Memahami dan menggunakan **16 Core Components** React Native
+2. Menerapkan **StyleSheet** untuk styling terpusat
+3. Menggunakan **useState** untuk state management dasar
+4. Membuat layout yang responsif dengan **Flexbox**
+5. Menangani **interaksi pengguna** (tekan, input, scroll)
+
+
+## 📲 Praktikum ##
+
+### Langkah 1: Import Libarary & Components ###
+
+1. Buka File App.js pada folder projek ptmn2
+2. Import Library dan Core Component yang dibutuhkan
+3. Konfirmasi bukti
+![alt text](image.png)
+
+### Langkah 2: Menyiapkan Array Objek untuk menampung data ###
+
+1. Membuat Array Objek bernama PROFILE untuk menampung data Profile
+2. Konfirmasi bukti
+![alt text](image-2.png)
+
+## Lanjutkan sampai selesai.... ##
+
+
+### Hasil Akhir ###
+
+<img src="CV App.gif" width="25%" >
